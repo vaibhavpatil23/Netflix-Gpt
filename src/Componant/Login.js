29b79f0e40933bdf1a5984776452f0a000075b25
vaibhav <vpatil7382@gmail.com>
2023-08-28 19:44:51 +0530

    create a sing up form

--- src/Componant/Login.js
+++ src/Componant/Login.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <div>
       <Header />
@@ -12,7 +17,17 @@ const Login = () => {
         />{" "}
       </div>
       <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-        <h1 className="font-bold text-3xl py-4">Sing in</h1>
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-2 w-full bg-slate-700"
+          />
+        )}
+        
         <input
           type="text"
           placeholder="Email Address"
@@ -24,8 +39,13 @@ const Login = () => {
           className="p-4 my-2 w-full bg-slate-700"
         />
         <button className="p-4 my-6 bg-red-800 w-full rounded-lg">
-          Sing in
+          {isSignInForm ? "Sign In" : "Sign Up"}{" "}
         </button>
+        <p className="p-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sing up now"
+            : "Already registered? Sign In Now."}
+        </p>
       </form>
     </div>
   );
