f4d04ba817d69114a7123159056e2c821c7b929a
vaibhav <vpatil7382@gmail.com>
2023-09-01 18:19:09 +0530

    all bug fix

--- src/Componant/Body.js
+++ src/Componant/Body.js
@@ -23,11 +23,17 @@ const Body = () => {
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        const { uid, email, displayName,photoURL } = user;
-        dispatch(addUser({ uid: uid, email: email, displayName: displayName , photoURL:photoURL }));
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
       } else {
-        dispatch(removeUser())
-
+        dispatch(removeUser());
       }
     });
   }, []);
