5c9204b6b8ea93e5976bce764c1e4e7dd268df2c
vaibhav <vpatil7382@gmail.com>
2023-09-04 18:12:25 +0530

    video contener

--- src/Componant/Videobackgraund.js
+++ src/Componant/Videobackgraund.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector } from "react-redux";
 
-const Videobackgraund = ({movieId}) => {
-  const trailerVideo = useSelector((store)=>store.movies?.trailerVideo);
+const Videobackgraund = ({ movieId }) => {
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
 
   // useMovieTrailer(movieId)
   return (
-    <div className='w-screen'>
- <iframe
- className='w-screen aspect-video'>
-  src={"https://www.youtube.com/embed" + trailerVideo?.key}
-  titel = "You Tubee video player"
-  allow = ""
-
- </iframe>
+    <div className="w-screen">
+      <iframe className="w-screen aspect-video">
+        src={"https://www.youtube.com/embed" + trailerVideo?.key}
+        titel = "You Tubee video player"
+        allow = " "
+      </iframe>
     </div>
-  )
-}
+  );
+};
 
-export default Videobackgraund
+export default Videobackgraund;
