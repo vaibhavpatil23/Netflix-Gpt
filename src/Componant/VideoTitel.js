c67e5c777f9997cbab4f45462ae641064d97cad5
vaibhav <vpatil7382@gmail.com>
2023-09-13 18:47:08 +0530

    initial commit

--- src/Componant/VideoTitel.js
+++ src/Componant/VideoTitel.js
@@ -6,7 +6,7 @@ const VideoTitel = ({ titel, overview }) => {
     <div className="pt-36 px-6">
       <h1 className="text-6xl font-bold">{titel}</h1>
       <p className="py-6 text-lg w-1/4">{overview}</p>
-      <div className="my-4">
+      <div className="my-4  ">
         <button className="bg-gray-500 text-white p-4 px-12 text-lg bg-opacity-50 rounded-lg">
           ⏯️ Play
         </button>
@@ -15,5 +15,4 @@ const VideoTitel = ({ titel, overview }) => {
     </div>
   );
 };
-
 export default VideoTitel;
