c9b957180f170d9a802ee9cbf8f3ea357e22ce5e
vaibhav <vpatil7382@gmail.com>
2023-09-20 19:05:14 +0530

    initial commit

--- src/Utils/validation.js
+++ src/Utils/validation.js
@@ -6,7 +6,7 @@ export const checkValidaData = (email, password) => {
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
   if (!isEmailValid) return "Email ID is not valid";
-  if (!isPasswordValid) return "Password is not valid";
+  if (!isPasswordValid) return "Password is not valid"
 
   return null;
 };
